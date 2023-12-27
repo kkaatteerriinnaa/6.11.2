9ab40698d11be0805e0dacf127c0ac4edbf89b68
kkaat <kkaat@KATERINA>
2023-12-27 18:47:19 +0200

    Добавьте файлы проекта.

---
+++ 6.11/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace _6._11
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = new ViewModel();
+
+            AlphaSlider.ValueChanged += OnSliderValueChanged;
+            RedSlider.ValueChanged += OnSliderValueChanged;
+            GreenSlider.ValueChanged += OnSliderValueChanged;
+            BlueSlider.ValueChanged += OnSliderValueChanged;
+
+            Button button = (Button)this.FindName("Add");
+            if (button != null)
+            {
+                button.Click += OnAddButtonClick;
+            }
+        }
+
+        private void OnSliderValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (AlphaSlider != null && RedSlider != null && GreenSlider != null && BlueSlider != null)
+            {
+                Color color = Color.FromArgb((byte)AlphaSlider.Value, (byte)RedSlider.Value, (byte)GreenSlider.Value, (byte)BlueSlider.Value);
+                this.Background = new SolidColorBrush(color);
+            }
+        }
+
+
+        private void OnAddButtonClick(object sender, RoutedEventArgs e)
+        {
+            Color color = ((SolidColorBrush)this.Background).Color;
+            string colorString = $"#{color.A:X2}{color.R:X2}{color.G:X2}{color.B:X2}";
+            ListBox listBox = (ListBox)this.FindName("ColorListBox");
+            listBox.Items.Add(colorString);
+        }
+    }
+}
---
+++ 6.11/ViewModel.cs
@@ -0,0 +1,90 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace _6._11
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private Color _selectedColor;
+        public Color SelectedColor
+        {
+            get { return _selectedColor; }
+            set
+            {
+                _selectedColor = value;
+                OnPropertyChanged("SelectedColor");
+            }
+        }
+
+        private ObservableCollection<Color> _colorList;
+        public ObservableCollection<Color> ColorList
+        {
+            get { return _colorList; }
+            set
+            {
+                _colorList = value;
+                OnPropertyChanged("ColorList");
+            }
+        }
+        public class RelayCommand : ICommand
+        {
+            private readonly Action<object> _execute;
+            private readonly Predicate<object> _canExecute;
+
+            
+            public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+            {
+                _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+                _canExecute = canExecute;
+            }
+
+            public bool CanExecute(object parameter)
+            {
+                return _canExecute == null || _canExecute(parameter);
+            }
+
+            public void Execute(object parameter)
+            {
+                _execute(parameter);
+            }
+
+            public event EventHandler CanExecuteChanged
+            {
+                add { CommandManager.RequerySuggested += value; }
+                remove { CommandManager.RequerySuggested -= value; }
+            }
+        }
+
+        public ICommand AddColorCommand { get; set; }
+
+        public ViewModel()
+        {
+            ColorList = new ObservableCollection<Color>();
+            AddColorCommand = new RelayCommand(AddColor, CanAddColor);
+        }
+
+        private void AddColor(object color)
+        {
+            ColorList.Add((Color)color);
+        }
+        private bool CanAddColor(object color)
+        {
+            return !ColorList.Contains((Color)color);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+}
